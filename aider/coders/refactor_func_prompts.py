68fc67a2e02b4c4d9ded1c4dbef9df76f20453b2
Jim White <jim@ifcx.org>
2023-11-28 14:00:48 -0800

    aider: Add function source code to refactor prompt.
    # Aider chat conversation:
    
    ASSISTANT: To include the source code of the function to be refactored in the prompt, we need to modify the `RefactorFunctionPrompts.refactor_function` method in `aider/coders/refactor_func_prompts.py`. We will adjust the method to concatenate the source code of the function within the prompt string.
    
    Here is the *SEARCH/REPLACE* block for the necessary change:
    
    aider/coders/refactor_func_prompts.py
    ```python
    <<<<<<< SEARCH
        @staticmethod
        def refactor_function(source_code, start_line, end_line):
            return (
                f"Please refactor the function in the following Python code. "
                f"Make sure the refactored function starts from line {start_line} "
                f"and ends at line {end_line}. Apply best coding practices and "
                f"improve the code structure without changing the functionality.\n\n"
                f"{source_code}"
            )
    =======
        @staticmethod
        def refactor_function(source_code, start_line, end_line, function_source):
            return (
                f"Please refactor the function in the following Python code. "
                f"Make sure the refactored function starts from line {start_line} "
                f"and ends at line {end_line}. Apply best coding practices and "
                f"improve the code structure without changing the functionality.\n\n"
                f"{function_source}\n\n"
                f"Here is the full source code for context:\n\n"
                f"{source_code}"
            )
    >>>>>>> REPLACE
    ```
    
    This change adds a new parameter `function_source` to the `refactor_function` method, which is the source code of the function to be refactored. The prompt now includes this source code and also provides the full source code for context.

--- aider/coders/refactor_func_prompts.py
+++ aider/coders/refactor_func_prompts.py
@@ -1,10 +1,12 @@
 class RefactorFunctionPrompts:
     @staticmethod
-    def refactor_function(source_code, start_line, end_line):
+    def refactor_function(source_code, start_line, end_line, function_source):
         return (
             f"Please refactor the function in the following Python code. "
             f"Make sure the refactored function starts from line {start_line} "
             f"and ends at line {end_line}. Apply best coding practices and "
             f"improve the code structure without changing the functionality.\n\n"
+            f"{function_source}\n\n"
+            f"Here is the full source code for context:\n\n"
             f"{source_code}"
         )
