1bc239e59f562daf9a7455847e9ecc6818993513
Jim White <jim@ifcx.org>
2023-11-28 14:02:29 -0800

    aider: Refactored the `RefactorFunctionCoder.generate_prompt` method to include line numbers in the prompt by extracting relevant lines of code from the source file.
    # Aider chat conversation:
    
    ASSISTANT: To include the source code with line numbers for files in the chat in the calls to the refactoring API, we need to modify the `RefactorFunctionCoder.generate_prompt` method in `aider/coders/refactor_func_coder.py`. We will adjust the method to extract the relevant lines of code from the source file, add line numbers to them, and then include this in the prompt.
    
    Here are the *SEARCH/REPLACE* blocks for the necessary changes:
    
    aider/coders/refactor_func_coder.py
    ```python
    <<<<<<< SEARCH
        def generate_prompt(self, source_file, start_line, end_line):
            # This method will generate the prompt for the model
            prompt = f"Refactor the function in the following Python code starting from line {start_line} to line {end_line}:\n\n"
            prompt += source_file
            return prompt
    =======
        def generate_prompt(self, source_file, start_line, end_line):
            # This method will generate the prompt for the model
            # Extract the relevant lines of code
            lines = source_file.split('\n')[start_line - 1:end_line]
            # Add line numbers to the extracted lines
            numbered_lines = [f"{i + start_line}: {line}" for i, line in enumerate(lines)]
            # Generate the prompt with line numbers
            prompt = (
                f"Refactor the function in the following Python code starting from line {start_line} to line {end_line}:\n\n"
                + "\n".join(numbered_lines)
            )
            return prompt
    >>>>>>> REPLACE
    ```
    
    This change adds the logic to split the source file into lines, extract the relevant lines for the function to be refactored, add line numbers to these lines, and then concatenate them into the prompt string. This will provide the model with the necessary context to perform the refactoring with line numbers for reference.

--- aider/coders/refactor_func_coder.py
+++ aider/coders/refactor_func_coder.py
@@ -3,8 +3,15 @@ from aider.coders.base_coder import Coder
 class RefactorFunctionCoder(Coder):
     def generate_prompt(self, source_file, start_line, end_line):
         # This method will generate the prompt for the model
-        prompt = f"Refactor the function in the following Python code starting from line {start_line} to line {end_line}:\n\n"
-        prompt += source_file
+        # Extract the relevant lines of code
+        lines = source_file.split('\n')[start_line - 1:end_line]
+        # Add line numbers to the extracted lines
+        numbered_lines = [f"{i + start_line}: {line}" for i, line in enumerate(lines)]
+        # Generate the prompt with line numbers
+        prompt = (
+            f"Refactor the function in the following Python code starting from line {start_line} to line {end_line}:\n\n"
+            + "\n".join(numbered_lines)
+        )
         return prompt
 
     def process_response(self, response):
